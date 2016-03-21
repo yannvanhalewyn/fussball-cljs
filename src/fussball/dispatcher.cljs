dc6d2c039d8d1e0efc8b971681d8a3d642c1b2eb
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-03-21 01:45:26 +0100

    Register default action (when action name was never handled)

--- src/fussball/dispatcher.cljs
+++ src/fussball/dispatcher.cljs
@@ -1,7 +1,9 @@
 (ns fussball.dispatcher
   (:require [fussball.store :as store]))
 
-(defmulti action (fn [action_type & params] action_type))
+(defmulti action (fn [action_type params] action_type))
+
+(defmethod action :default [action_type params] (js/alert (str "No action registered for " action_type)))
 
 (defn dispatch [action_type & params]
   (let [payload (action action_type params)]
